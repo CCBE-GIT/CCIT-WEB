988002f1e1a175c09dc4773d0cc1bf4995db523e
Y. A. Perera <yunethperera987@gmail.com>
2026-02-24 09:33:47 +0530

    update Background Video Link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <!-- Video Background -->
       <div class="video-bg">
         <video ref="videoBg" autoplay muted loop playsinline>
-        <source src="/videos/typing video.mp4" type="video/mp4">
+        <source src="https://ik.imagekit.io/kp5tixhur/ccit-video/typing%20video.mp4" type="video/mp4">
           Your browser does not support the video tag.
         </video>
         <div class="video-overlay"></div>
